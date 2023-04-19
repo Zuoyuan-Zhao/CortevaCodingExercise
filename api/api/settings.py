e5aba5ddeb36bced004feefdccf96a32cd0d66a4
zuoyuan <zuoyuan-zhao@outlook.com>
2023-04-19 00:52:49 -0500

    update credentials

--- api/api/settings.py
+++ api/api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lhawa5%p-7y(i+m)s4)h2-a3xow#cy1#u1uvk^nc7+6wwk8945'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
